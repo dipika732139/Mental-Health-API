78d5d4592856f9e3c4d8d9b6a1ec1474bf709a4c
Dipika Mandal <dmandal@affinionds.com>
2020-06-01 08:37:14 +0530

    Created separe classes for core models

--- src/Mental.Health.Model/Models/Question.cs
+++ src/Mental.Health.Model/Models/Question.cs
@@ -11,22 +11,4 @@ namespace Mental.Health
         public string QuestionData { get; set; }
         public List<Option> Options { get; set; }
     }
-    public class Answer
-    {
-        public string UserId { get; set; }
-        public string MentalHealthTestId { get; set; }
-        public string TestId { get; set; }
-        public int QuestionNumber { get; set; }
-        public int ChosenOption { get; set; }
-    }
-    public class Result
-    {
-        public string UserId { get; set; }
-        public string MentalHealthTestId { get; set; }
-        public string TestId { get; set; }
-        public string MoodImageUrl { get; set; }
-        public int Score { get; set; }
-        public string Description { get; set; }
-        public string Summary { get; set; }
-    }
 }
