7e76c9d4482d0597aed968a7e3bee92916529016
Dipika Mandal <dmandal@affinionds.com>
2020-06-06 10:23:01 +0530

    added cors setup

--- src/Mental.Health.Web/Startup.cs
+++ src/Mental.Health.Web/Startup.cs
@@ -45,17 +45,7 @@ namespace Mental.Health.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
+            app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
             app.UseCustomExceptionHandler();
             app.UseMvc();
         }
