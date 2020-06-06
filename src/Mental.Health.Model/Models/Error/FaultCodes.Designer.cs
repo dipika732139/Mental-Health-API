1e0c84ae05cbd29f4cda9189cde6d6831f9c5caa
Dipika Mandal <dmandal@affinionds.com>
2020-06-06 10:23:55 +0530

    added faultcodes for password length

--- src/Mental.Health.Model/Models/Error/FaultCodes.Designer.cs
+++ src/Mental.Health.Model/Models/Error/FaultCodes.Designer.cs
@@ -19,7 +19,7 @@ namespace Mental.Health.Model.Models.Error {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class FaultCodes {
@@ -120,6 +120,15 @@ namespace Mental.Health.Model.Models.Error {
         public static string MissingField {
             get {
                 return ResourceManager.GetString("MissingField", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 11.
+        /// </summary>
+        public static string PasswordTooShort {
+            get {
+                return ResourceManager.GetString("PasswordTooShort", resourceCulture);
             }
         }
         
